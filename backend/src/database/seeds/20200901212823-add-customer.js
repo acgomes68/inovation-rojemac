91d46ca6d703137c29f2667173c84b1f5e3e4e2f
acgomes68 <acgomes68@gmail.com>
2020-09-23 16:58:39 -0300

    Atualização das dependências, criação do yarn.lock e correção de lint no seed de customers

--- backend/src/database/seeds/20200901212823-add-customer.js
+++ backend/src/database/seeds/20200901212823-add-customer.js
@@ -1,39 +1,23 @@
 module.exports = {
-  up: (queryInterface) =>
-    /*
-    Add altering commands here.
-    Return a promise to correctly handle asynchronicity.
-
-    Example:
-    return queryInterface.bulkDelete('People', {'name': John Deo'}, {});
-    */
-    queryInterface.bulkInsert(
-      'customers',
-      [
-        {
-          cnpj: '09181426000163',
-          name: 'CONVERDIG TECNOLOGIA EM INFORMATICA LTDA',
-          address: 'PC MARECHAL DEODORO',
-          address_number: 427,
-          address_complement: 'Apt 2',
-          city: 'SÃO PAULO',
-          state: 'SP',
-          zip: '01150011',
-          phone: '+5511932291835',
-          email: 'acgomes@gmail.com',
-          created_at: new Date(),
-        },
-      ],
-      {},
-    ),
-  down: (queryInterface) =>
-    /*
-    Add reverting commands here.
-    Return a promise to correctly handle asynchronicity.
-
-    Example:
-    return queryInterface.bulkDelete('People', null, {});
-    */
-    queryInterface.bulkDelete('customers', null, {})
+  up: (queryInterface) => queryInterface.bulkInsert(
+    'customers',
+    [
+      {
+        cnpj: '09181426000163',
+        name: 'CONVERDIG TECNOLOGIA EM INFORMATICA LTDA',
+        address: 'PC MARECHAL DEODORO',
+        address_number: 427,
+        address_complement: 'Apt 2',
+        city: 'SÃO PAULO',
+        state: 'SP',
+        zip: '01150011',
+        phone: '+5511932291835',
+        email: 'acgomes@gmail.com',
+        created_at: new Date(),
+      },
+    ],
+    {},
+  ),
+  down: (queryInterface) => queryInterface.bulkDelete('customers', null, {})
   ,
 };
